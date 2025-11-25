6595409e8b1ebfef6b797053ef4c424e51145bc2
dan <danieldoe1ee7@icloud.com>
2025-11-25 22:14:10 +0300

    Initial Commit

---
+++ findDevice/findDeviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  findDeviceApp.swift
+//  findDevice
+//
+//  Created by Daniel on 25.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct findDeviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
